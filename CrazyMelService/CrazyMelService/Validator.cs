c9dad28e0143150129752dd8e80f3440fb853b4b
Adrian Krebs <adgekrebs@gmail.com>
2014-11-21 17:55:57 -0500

    added Validator class

--- CrazyMelService/CrazyMelService/Validator.cs
+++ CrazyMelService/CrazyMelService/Validator.cs
@@ -7,5 +7,55 @@ namespace CrazyMelService
 {
     public class Validator
     {
+    	public Validator()
+    	{
+
+    	}
+
+    	public static bool ValidateVarChar(string input, int size)
+    	{
+    		if (input.Length > size)
+    		{
+    			return false;
+    		}
+    		return true;
+    	}
+    	public static bool ValidateBool(string input)
+    	{
+    		try
+    		{
+    			Convert.ToBoolean(input);
+    		}
+    		catch(Exception e)
+    		{
+    			return false;
+    		}
+    		return true;
+    	}
+    	public static  bool ValidateInt(string input)
+    	{
+    		try
+    		{
+    			Convert.ToInt32(input);
+    		}
+    		catch(Exception e)
+    		{
+    			return false;
+    		}
+    		return true;
+    	}
+    	public static  bool ValidateFloat(string input)
+    	{
+    		try
+    		{
+    			Convert.ToSingle(input);
+    		}
+    		catch(Exception e)
+    		{
+    			return false;
+    		}
+    		return true;
+    	}
+	}
     }
 }
