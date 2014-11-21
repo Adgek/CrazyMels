dbbb6f5c44d3dfc8a04e412803411ed67ee3b6e8
Matthew Anselmo <matthewanselmo@outlook.com>
2014-11-21 13:43:52 -0800

    added validator and db calssses

---
+++ CrazyMelService/CrazyMelService/Database.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrazyMelService
+{
+    public class Database
+    {
+        private string username { get; set; }
+        private string password { get; set; }
+        private string server { get; set; }
+        private string databaseName { get; set; }
+
+        public Database() { }
+        public Database(string Username, string Password, string Server, string DatabaseName)
+        {
+            username = Username;
+            password = Password;
+            server = Server;
+            databaseName = DatabaseName;
+        }
+    }
+}
