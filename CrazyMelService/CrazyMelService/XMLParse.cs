58529bb66ee133e79838be99bcc85ca437c6edb9
Matthew Anselmo <matthewanselmo@outlook.com>
2014-11-22 12:55:27 -0500

    Added XML formatting requirement to XMLParse Class

--- CrazyMelService/CrazyMelService/XMLParse.cs
+++ CrazyMelService/CrazyMelService/XMLParse.cs
@@ -65,3 +65,39 @@ namespace CrazyMelService
         }
     }
 }
+
+/*
+
+<Object>
+  <Type>Customer</Type>
+  <CustID>1</CustID>
+  <FirstName>matt</FirstName>
+  <LastName>anselmo</LastName>
+  <PhoneNumber>2267501111</PhoneNumber>  
+</Object>
+
+<Object>
+  <Type>Product</Type>
+  <ProdID>1</ProdID>
+  <ProdName>banana</ProdName>
+  <Price>1.9</Price>
+  <ProdWeight>.1</ProdWeight>
+  <InStock>1</InStock>
+</Object>
+
+<Object>
+  <Type>Order</Type>
+  <OrderID>1</OrderID>
+  <CustID>1</CustID>
+  <PoNumber>asdasdasdasd</PoNumber>
+  <OrderDate>01-01-1999</OrderDate>
+</Object>
+
+<Object>
+  <Type>Cart</Type>
+  <OrderID>1</OrderID>
+  <ProdID>matt</ProdID>
+  <Quantity>anselmo</Quantity>  
+</Object>
+
+*/
