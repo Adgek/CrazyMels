dbf656622e1aa47bc652f132b137a06dfb049988
ktfowler1337 <fowler_xx@hotmail.com>
2014-11-22 21:11:50 -0500

    updated files

---
+++ myThing/Soa4/Models/REST.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+
+namespace Soa4.Models
+{
+    public class REST
+    {
+        public string MakeRequest(string xmlinput, string objectType,string crudMethod, string method)
+        {
+            // Restful service URL
+            string url = "http://localhost:57871/CrazyMel.svc/" + crudMethod + objectType;
+
+            // declare ascii encoding
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            string strResult = string.Empty;
+
+            string postData = xmlinput.ToString();
+            // convert xmlstring to byte using ascii encoding
+            byte[] data = encoding.GetBytes(postData);
+            // declare httpwebrequet wrt url defined above
+            HttpWebRequest webrequest = (HttpWebRequest)WebRequest.Create(url);
+            // set method as post
+            webrequest.Method = method;
+            // set content type
+            webrequest.ContentType = "application/x-www-form-urlencoded";
+            // set content length
+            webrequest.ContentLength = data.Length;
+            // get stream data out of webrequest object
+            Stream newStream = webrequest.GetRequestStream();
+            newStream.Write(data, 0, data.Length);
+            newStream.Close();
+            // declare & read response from service
+            HttpWebResponse webresponse = (HttpWebResponse)webrequest.GetResponse();
+
+            // set utf8 encoding
+            Encoding enc = System.Text.Encoding.GetEncoding("utf-8?");
+            // read response stream from response object
+            StreamReader loResponseStream =
+        new StreamReader(webresponse.GetResponseStream(), enc);
+            // read string from stream data
+            strResult = loResponseStream.ReadToEnd();
+            // close the stream object
+            loResponseStream.Close();
+            // close the response object
+            webresponse.Close();
+            // below steps remove unwanted data from response string
+            strResult = strResult.Replace("</string>", "");
+            return strResult;
+        }
+    }
+}
